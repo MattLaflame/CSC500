d6a771ee2eb7e3c041a6922b2b60b2ee1850336c
MattLaflame <matthew.laflame@csuglobal.edu>
2025-11-16 10:36:20 -0800

    Add files via upload

---
+++ CSC500-CT-1-PART1.py
@@ -0,0 +1,6 @@
+num1 = int(input('Enter first integer:\n'))
+num2 = int(input('Enter second integer:\n'))
+sum = num1 + num2
+print('The sum of', num1, 'and', num2, 'is', sum)
+subtraction = num1 - num2
+print('The subtraction of', num1, 'and', num2, 'is', subtraction)
---
+++ CSC500-CT-1-PART2.py
@@ -0,0 +1,6 @@
+num1 = int(input('Enter first integer:\n'))
+num2 = int(input('Enter second integer:\n'))
+multiplication = num1 * num2
+print('The multiplication of', num1, 'and', num2, 'is', multiplication)
+division = num1 / num2
+print('The division of', num1, 'and', num2, 'is', division)
